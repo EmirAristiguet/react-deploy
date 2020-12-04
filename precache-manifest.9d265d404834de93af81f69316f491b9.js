2e86114918a69a16251529d0efb84966e3ca8915
unknown <miguel1993mo@gmail.com>
2020-12-04 14:45:29 -0400

    Updates

---
+++ precache-manifest.9d265d404834de93af81f69316f491b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d672abe0640f693cbd3a35a3484aead6",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "40aa74c3c7299b3ba696",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a225d0adb3fdbbc49102",
+    "url": "/react-deploy/static/js/2.67efc972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.67efc972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40aa74c3c7299b3ba696",
+    "url": "/react-deploy/static/js/main.22316d59.chunk.js"
+  },
+  {
+    "revision": "90d230b4f7e595398471",
+    "url": "/react-deploy/static/js/runtime-main.5e53320b.js"
+  }
+]);
